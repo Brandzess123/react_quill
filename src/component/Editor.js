582b8381a50c96d2ae1dbd2b2074fd26b89644eb
brandzess123 <lolikimngok@gmail.com>
2023-07-03 16:39:58 +0700

    add ckeditor

---
+++ src/component/Editor.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef } from "react";
+
+function Editor({ onChange, editorLoaded, name, value }) {
+  const editorRef = useRef();
+  const { CKEditor, ClassicEditor } = editorRef.current || {};
+
+  useEffect(() => {
+    editorRef.current = {
+      CKEditor: require("@ckeditor/ckeditor5-react").CKEditor, // v3+
+      ClassicEditor: require("@ckeditor/ckeditor5-build-classic"),
+    };
+  }, []);
+
+  return (
+    <div>
+      {editorLoaded ? (
+        <CKEditor
+          type=""
+          name={name}
+          editor={ClassicEditor}
+          data={value}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            // console.log({ event, editor, data })
+            onChange(data);
+
+            editor.editing.view.change((writer) => {
+              writer.setStyle(
+                "height",
+                "1200px",
+                editor.editing.view.document.getRoot()
+              );
+            });
+          }}
+        />
+      ) : (
+        <div>Editor loading</div>
+      )}
+    </div>
+  );
+}
+
+export default Editor;
--- src/pages/ckeditor/index.js
+++ src/pages/ckeditor/index.js
@@ -1,1 +1,34 @@
+import React, { useState, useEffect } from "react";
+// import "./styles.css";
+import Editor from "../../component/Editor";
+import Render from "../../component/render";
 
+export default function App() {
+  const [editorLoaded, setEditorLoaded] = useState(false);
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    setEditorLoaded(true);
+  }, []);
+  console.log(data);
+
+  return (
+    <div className="App">
+      <h1>ckEditor 5</h1>
+
+      <Editor
+        name="description"
+        onChange={(data) => {
+          setData(data);
+        }}
+        editorLoaded={editorLoaded}
+      />
+
+      {JSON.stringify(data)}
+
+      <div className="mt-[300px] w-full h-[500px] border bg-black text-white">
+        <Render htmlString={data} />
+      </div>
+    </div>
+  );
+}
