0d77c338f01a0abe900ea87abdc871102b71d2dc
brandzess123 <lolikimngok@gmail.com>
2023-07-03 11:28:25 +0700

    A

--- src/pages/draft/index.js
+++ src/pages/draft/index.js
@@ -1,129 +1,18 @@
-import React, { Component } from "react";
-import { convertFromRaw, EditorState } from "draft-js";
+// import Editor from "@draft-js-plugins/editor";
 
-import Editor, { composeDecorators } from "@draft-js-plugins/editor";
+// import createImagePlugin from "@draft-js-plugins/image";
+// import React from "react";
 
-import createImagePlugin from "@draft-js-plugins/image";
+// const imagePlugin = createImagePlugin();
 
-import createAlignmentPlugin from "@draft-js-plugins/alignment";
+// // The Editor accepts an array of plugins. In this case, only the imagePlugin
+// // is passed in, although it is possible to pass in multiple plugins.
+// const MyEditor = ({ editorState, onChange }) => (
+//   <Editor
+//     editorState={editorState}
+//     onChange={onChange}
+//     plugins={[imagePlugin]}
+//   />
+// );
 
-import createFocusPlugin from "@draft-js-plugins/focus";
-
-import createResizeablePlugin from "@draft-js-plugins/resizeable";
-
-import createBlockDndPlugin from "@draft-js-plugins/drag-n-drop";
-
-import createDragNDropUploadPlugin from "@draft-js-plugins/drag-n-drop-upload";
-import editorStyles from "./editorStyles.module.css";
-import mockUpload from "./mockUpload";
-
-const focusPlugin = createFocusPlugin();
-const resizeablePlugin = createResizeablePlugin();
-const blockDndPlugin = createBlockDndPlugin();
-const alignmentPlugin = createAlignmentPlugin();
-const { AlignmentTool } = alignmentPlugin;
-
-const decorator = composeDecorators(
-  resizeablePlugin.decorator,
-  alignmentPlugin.decorator,
-  focusPlugin.decorator,
-  blockDndPlugin.decorator
-);
-const imagePlugin = createImagePlugin({ decorator });
-
-const dragNDropFileUploadPlugin = createDragNDropUploadPlugin({
-  handleUpload: mockUpload,
-  addImage: imagePlugin.addImage,
-});
-
-const plugins = [
-  dragNDropFileUploadPlugin,
-  blockDndPlugin,
-  focusPlugin,
-  alignmentPlugin,
-  resizeablePlugin,
-  imagePlugin,
-];
-
-/* eslint-disable */
-const initialState = {
-  entityMap: {
-    0: {
-      type: "IMAGE",
-      mutability: "IMMUTABLE",
-      data: {
-        src: "/images/canada-landscape-small.jpg",
-      },
-    },
-  },
-  blocks: [
-    {
-      key: "9gm3s",
-      text: "You can have images in your text field. This is a very rudimentary example, but you can enhance the image plugin with resizing, focus or alignment plugins.",
-      type: "unstyled",
-      depth: 0,
-      inlineStyleRanges: [],
-      entityRanges: [],
-      data: {},
-    },
-    {
-      key: "ov7r",
-      text: " ",
-      type: "atomic",
-      depth: 0,
-      inlineStyleRanges: [],
-      entityRanges: [
-        {
-          offset: 0,
-          length: 1,
-          key: 0,
-        },
-      ],
-      data: {},
-    },
-    {
-      key: "e23a8",
-      text: "See advanced examples further down …",
-      type: "unstyled",
-      depth: 0,
-      inlineStyleRanges: [],
-      entityRanges: [],
-      data: {},
-    },
-  ],
-};
-/* eslint-enable */
-
-export default class CustomImageEditor extends Component {
-  state = {
-    editorState: EditorState.createWithContent(convertFromRaw(initialState)),
-  };
-
-  onChange = (editorState) => {
-    this.setState({
-      editorState,
-    });
-  };
-
-  focus = () => {
-    this.editor.focus();
-  };
-
-  render() {
-    return (
-      <div>
-        <div className={editorStyles.editor} onClick={this.focus}>
-          <Editor
-            editorState={this.state.editorState}
-            onChange={this.onChange}
-            plugins={plugins}
-            ref={(element) => {
-              this.editor = element;
-            }}
-          />
-          <AlignmentTool />
-        </div>
-      </div>
-    );
-  }
-}
+// export default MyEditor;
--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -1,21 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuill } from "react-quilljs";
 import "quill/dist/quill.snow.css";
+import BlotFormatter from "quill-blot-formatter/dist/BlotFormatter";
 
 function HtmlEditor() {
-  const { quill, quillRef } = useQuill();
+  //b1
+  // const { quill, quillRef } = useQuill();
+  const { quill, quillRef, Quill } = useQuill({
+    modules: { blotFormatter: {} },
+  });
+  if (Quill && !quill) {
+    Quill.register("modules/blotFormatter", BlotFormatter);
+  }
+  //==========================================================
+
   const [value, setValue] = useState();
 
-  React.useEffect(() => {
+  //b2
+  useEffect(() => {
     if (quill) {
-      quill.on("text-change", () => {
+      quill.on("text-change", (delta, oldContents) => {
         console.log(quillRef.current.firstChild.innerHTML);
         setValue(quillRef.current.firstChild.innerHTML);
+
+        let currrentContents = quill.getContents();
+        console.log(currrentContents.diff(oldContents));
       });
     }
-  }, [quill]);
+  }, [quill, Quill]);
 
-  console.log(value, "this is quill editor");
+  // console.log(value);
+  console.log(JSON.stringify(value));
+  // console.log(quillRef);
   return (
     <div>
       <div className="mt-[50px] mx-auto " style={{ width: 1500, height: 1300 }}>
